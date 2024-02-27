659c9773e75de972da6cd1bafbd52e9e7249a6a3
ALEX VALERIANO <alansuzumiya@gmail.com>
2024-02-27 15:43:37 -0400

    Agregar archivos de proyecto.

---
+++ practica_1/perro.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "mamifero.h"
+class perro :public mamifero
+{
+public:
+    perro(string _tipo) : mamifero(_tipo) {}
+
+    void hacerSonido() const override {
+        cout << "Guau!" << endl;
+    }
+
+    string getCategoria() const override {
+        return "Perro " + mamifero::getCategoria();
+    }
+};
+
